fc5184623238407cc343038ac41b72c3fdbc41ec
Ishita Karna <ishitakarna@pop-os.localdomain>
2020-03-28 16:47:12 +0530

    Research

--- func.h
+++ func.h
@@ -1,33 +1,17 @@
-//----Spinlock----
+/*---Typedef's---*/
 typedef struct spinlock_t {
     int x;
 }spinlock_t;
 
-int test_and_set(spinlock_t *s);
-void spin_lock(spinlock_t *s);
-void spin_unlock (spinlock_t *s);
-
-//----Condition----
 typedef struct condition {
     spinlock_t listLock;
 }condition;
 
-//----Semaphores----
 typedef struct semaphore {
     int x;
     spinlock_t sl;
 }semaphore;
 
-void initsem(semaphore *sem, int val);
-void P(semaphore *sem); //acquire semaphore
-void V(semaphore *sem); //release semaphore 
-
-//---Functionality---
-void wait(condition *c, spinlock_t *s);
-void do_signal(condition *c); //wake up one thread
-void do_broadcast(condition *c); //wake up all threads 
-
-//----Read Write Locks----
 typedef struct rwlock {
     int nActive; //number of active readers, -1 if writer is active
     int nPendingReads;
@@ -37,6 +21,24 @@ typedef struct rwlock {
     condition canWrite;
 }rwlock;
 
+/*---Functions---*/
+
+//Spinlock
+int test_and_set(spinlock_t *s);
+void spin_lock(spinlock_t *s);
+void spin_unlock (spinlock_t *s);
+
+//Semaphore
+void initsem(semaphore *sem, int val);
+void P(semaphore *sem); //acquire semaphore
+void V(semaphore *sem); //release semaphore 
+
+//Functionality
+void wait(condition *c, spinlock_t *s);
+void do_signal(condition *c); //wake up one thread
+void do_broadcast(condition *c); //wake up all threads 
+
+//Read write locks
 void lockShared(rwlock *r);
 void unlockShared(rwlock *r);
 void lockExclusive(rwlock *r);
